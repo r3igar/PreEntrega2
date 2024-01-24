3258ffbd8055e00c51e635061bab6c40b93f3a51
r3igar <ariel.munioz1995@gmail.com>
2024-01-24 19:39:01 -0300

    PreEntrega3

--- main.js
+++ main.js
@@ -1,43 +1,58 @@
-// Función costo total de productos
+// Función costo total
 function calcularCostoTotal(producto, cantidad) {
     const precioUnitario = obtenerPrecioUnitario(producto);
     const costoTotal = calcularCosto(precioUnitario, cantidad);
+
+    // Almacenar costo total Storage
+    localStorage.setItem('costoTotal', costoTotal);
+
     return costoTotal;
 }
 
-// Función precio unitario
+// precio unitario
 function obtenerPrecioUnitario(producto) {
     switch (producto) {
         case 1:
-            return 100; 
+            return 100;
         case 2:
-            return 150; 
+            return 150;
         case 3:
-            return 200; 
+            return 200;
         default:
-            return 0; 
+            return 0;
     }
 }
 
-// Función calcular costo total
+// calcular costo total
 function calcularCosto(precioUnitario, cantidad) {
     const iva = 0.21;
     const costoConIva = precioUnitario * cantidad * (1 + iva);
-    const descuento = 50; 
+    const descuento = 50;
     const costoTotal = costoConIva - descuento;
     return costoTotal;
 }
 
 // Lógica principal
-let productoSeleccionado = parseInt(prompt("Seleccione un producto: 1, 2, o 3"));
-let cantidadSeleccionada = parseInt(prompt("Ingrese la cantidad deseada"));
+document.addEventListener('DOMContentLoaded', function () {
+    const calcularButton = document.getElementById('calcularButton');
+    calcularButton.addEventListener('click', function () {
+        let productoSeleccionado = parseInt(document.getElementById('productoInput').value);
+        let cantidadSeleccionada = parseInt(document.getElementById('cantidadInput').value);
 
-// Validar que la cantidad sea un número positivo
-if (isNaN(cantidadSeleccionada) || cantidadSeleccionada <= 0) {
-    alert("Ingrese una cantidad válida.");
-} else {
-    // Calcular y mostrar el costo total
-    const costoTotal = calcularCostoTotal(productoSeleccionado, cantidadSeleccionada);
-    console.log("El costo total es: " + costoTotal);
-    alert("El costo total es: " + costoTotal);
-}
+ 
+        if (isNaN(cantidadSeleccionada) || cantidadSeleccionada <= 0) {
+            alert("Ingrese una cantidad válida.");
+        } else {
+            // Calcular y mostrar total
+            const costoTotal = calcularCostoTotal(productoSeleccionado, cantidadSeleccionada);
+            console.log("El costo total es: " + costoTotal);
+            alert("El costo total es: " + costoTotal);
+        }
+    });
+
+    // Recuperar el costo total del Storage
+    const costoTotalGuardado = localStorage.getItem('costoTotal');
+    if (costoTotalGuardado) {
+        console.log("Costo total recuperado del Storage: " + costoTotalGuardado);
+    }
+});
